78fb3b148fcd5e6b16554841a196db698aa5ee7f
Lucas Conte <lucascontee@icloud.com>
2026-04-10 20:27:27 -0300

    feat: added services configuration on program

--- Program.cs
+++ Program.cs
@@ -1,16 +1,40 @@
-﻿using System;
-using Avalonia;
+﻿using Avalonia;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SongsInLearning.ViewModels;
+using System;
+using System.Net;
+using System.Text;
 
 namespace SongsInLearning
 {
     internal class Program
     {
+        public static IHost AppHost { get; set; }
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args) 
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+
+            AppHost = Host.CreateDefaultBuilder(args)               
+               .ConfigureServices((context, services) =>
+               {
+                   services.AddSingleton(context.Configuration);
+                   services.AddTransient<MainViewModel>();
+
+               })
+               .Build();
+          
+                BuildAvaloniaApp()
+                    .StartWithClassicDesktopLifetime(args);
+
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
